1eb52547e0856fada1d0f1c80af79ddc132c923f
akankshjoshi <97383953+akankshjoshi@users.noreply.github.com>
2023-03-23 10:09:16 +0530

    code for students

---
+++ StringFunction.java
@@ -0,0 +1,18 @@
+class StringFunction
+{
+  public static void main(String args[])
+{
+  String s = "Akanksh";
+  System.out.println(s.toUpperCase());
+  System.out.println(s.toLowerCase());
+  System.out.println(s.trim());
+  System.out.println(s.startsWith("Sa"));
+  System.out.println(s.endsWith("n"));
+  System.out.println(s.charAt(0));
+  System.out.println(s.charAt(6));
+  System.out.println(s.length());
+  System.out.println(s);
+}
+}
+
+  
